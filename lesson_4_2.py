83e8d0c8074c0817ae50e0bdfe914844026de45b
Igor <igorg789170@gmail.com>
2026-04-07 21:05:33 +0600

    Добавил примеры магических методов

---
+++ lesson_4_2.py
@@ -0,0 +1,28 @@
+class Playlist:
+    # магические методы, dunder - double underscore
+    def __init__(self, name, songs=None):
+        self.name = name
+        self.songs = songs
+
+    def __str__(self):
+        return f"<Playlist object, name: {self.name}>"
+
+    def __len__(self):
+        return len(self.songs)
+
+    def __contains__(self, item):
+        print("items in contains", item)
+        return item in self.songs
+
+    def __bool__(self):
+        return len(self.songs) > 0
+
+pop = Playlist("pop playlist", songs=["Shape of my heart", "Bad guy"])
+print(pop)
+print(len(pop))
+print("Billy Jiens" in pop)
+print("Shape of my heart" in pop)
+if pop:
+    print("Playlist is not empty")
+else:
+    print("Playlist is empty")
