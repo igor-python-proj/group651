613d1e87cb6f50e5e5ed464f7e55f89cbd5df892
Igor <igorg789170@gmail.com>
2026-04-19 23:08:49 +0600

    Работа с данными

---
+++ lessons/database.py
@@ -0,0 +1,24 @@
+
+def create_tables(conn):
+    conn.execute("DROP TABLE IF EXISTS students")
+    conn.execute("""
+    CREATE TABLE IF NOT EXISTS students (
+        name TEXT,
+        age INTEGER,
+        city TEXT
+    )
+    """)
+
+def add_student(conn, name, age, city):
+    print(name, age, city)
+    conn.execute("""
+    INSERT INTO students (name, age, city)
+    VALUES (?, ?, ?)
+    """, (name, age, city))
+    conn.commit()
+
+def get_all_students(conn):
+    result = conn.execute("""
+    SELECT * FROM students
+    """)
+    return result.fetchall()
--- main.py
+++ main.py
@@ -1,4 +1,29 @@
 from lessons.playlist import Playlist
+from lessons.database import (
+    create_tables,
+    add_student,
+    get_all_students
+)
+import sqlite3
 
-play = Playlist("pop-2")
-print(play)
+# play = Playlist("pop-2")
+# print(play)
+connection = sqlite3.connect('database.db')
+create_tables(connection)
+add_student(
+    connection,
+    "Igor",
+    35,
+    "Bishkek",
+)
+add_student(
+    connection,
+    "Kurmanbek",
+    20,
+    "Karakol",
+)
+# for st in get_all_students(connection):
+#     print(st)
+print("== select ==")
+print(get_all_students(connection))
+connection.close()
