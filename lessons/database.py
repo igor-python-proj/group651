3d07b01b10edb1c56da6c007e7a26e250216b129
Igor <igorg789170@gmail.com>
2026-04-21 22:28:37 +0600

    Добавил запросы с select, delete, update, where

--- lessons/database.py
+++ lessons/database.py
@@ -3,7 +3,8 @@ def create_tables(conn):
     conn.execute("DROP TABLE IF EXISTS students")
     conn.execute("""
     CREATE TABLE IF NOT EXISTS students (
-        name TEXT,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
         age INTEGER,
         city TEXT
     )
@@ -18,7 +19,51 @@ def add_student(conn, name, age, city):
     conn.commit()
 
 def get_all_students(conn):
+    # result = conn.execute("""
+    # SELECT name FROM students
+    # """)
     result = conn.execute("""
     SELECT * FROM students
     """)
     return result.fetchall()
+
+def get_one_student(conn, student_id):
+    result = conn.execute("""
+    SELECT * FROM students WHERE id = ?
+    """, (student_id,))
+    return result.fetchall()
+
+def get_students_by_name(conn, name):
+    result = conn.execute("""
+    SELECT * FROM students WHERE name = ?
+    """, (name,))
+    return result.fetchall()
+
+def delete_student(conn, student_id):
+    conn.execute("""
+    DELETE FROM students WHERE id = ?
+    """, (student_id,))
+    conn.commit()
+
+def delete_by_city(conn, city):
+    conn.execute("""
+    DELETE FROM students WHERE city = ?
+    """, (city,))
+    conn.commit()
+
+def change_student(
+    conn,
+    student_id,
+    name,
+    age,
+    city,
+):
+    conn.execute("""
+    UPDATE students SET 
+    name = ?, 
+    age = ?, 
+    city = ? 
+    WHERE id = ?             
+    """,
+    (name, age, city, student_id))
+    conn.commit()
--- main.py
+++ main.py
@@ -2,9 +2,15 @@ from lessons.playlist import Playlist
 from lessons.database import (
     create_tables,
     add_student,
-    get_all_students
+    get_all_students,
+    get_one_student,
+    get_students_by_name,
+    delete_student,
+    delete_by_city,
+    change_student
 )
 import sqlite3
+from colorama import Fore
 
 # play = Playlist("pop-2")
 # print(play)
@@ -20,10 +26,56 @@ add_student(
     connection,
     "Kurmanbek",
     20,
+    "Bishkek",
+)
+add_student(
+    connection,
+    "Akylay",
+    21,
+    "Bishkek",
+)
+add_student(
+    connection,
+    "Burul",
+    22,
     "Karakol",
+)
+add_student(
+    connection,
+    "Kubat",
+    23,
+    "Naryn",
 )
 # for st in get_all_students(connection):
 #     print(st)
 print("== select ==")
 print(get_all_students(connection))
+one_student = get_one_student(
+    connection, 2
+)
+print(one_student)
+by_name = get_students_by_name(
+    connection, "Kurmanbek"
+)
+print(by_name)
+
+
+print("== delete ==")
+delete_student(connection, 1)
+print(get_all_students(connection))
+print("---")
+delete_by_city(connection, "Bishkek")
+print(get_all_students(connection))
+
+print("== update ==")
+change_student(
+    connection,
+    4,
+    "Burul",
+    21,
+    "Naryn",
+)
+print(Fore.RED + 'some red text')
+print(get_one_student(connection, 4))
 connection.close()
+
